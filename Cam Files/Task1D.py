f14722ac7a21baede9b44a3af08fbb03b4e277c1
Donald Ong <mho24@cam.ac.uk>
2017-01-26 23:25:57 +0000

    1D
    Revised rivers with stations

--- Cam Files/Task1D.py
+++ Cam Files/Task1D.py
@@ -2,11 +2,14 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.geo import rivers_with_station
 
 stations=build_station_list()
-def run():
-    dict =rivers_with_station(stations)
-    rivers=[]
-    for river in dict:
-        rivers.append(river)
-    rivers.sort()
-    return print (rivers[:10])
-run()
+#def run():
+#    dict =rivers_with_station(stations)
+#    rivers=[]
+#    for river in dict:
+#        rivers.append(river)
+#    rivers.sort()
+#    return print (rivers[:10])
+#run()
+rivers=rivers_with_station(stations)
+rivers.sort()
+print(rivers[:10])
--- Cam Files/floodsystem/geo.py
+++ Cam Files/floodsystem/geo.py
@@ -4,10 +4,19 @@ geographical data.
 """
 
 from floodsystem.utils import sorted_by_key
-#maps river names to a list of stations on a given river
+#returns all rivers (by name) with a monitoring station
 def rivers_with_station(stations):
+    rivers_with_station=[]
+    for station in stations:
+        rivers_with_station.append(station.river)
+#did not use set
+    return rivers_with_station
+
+#maps river names (the ‘key’) to a list of stations on a given river
+def stations_by_river(stations):
     dict={}
     for station in stations:
         dict[station.river]=station.name
     return dict
+
 #can improve because some river names begins with 'River'
