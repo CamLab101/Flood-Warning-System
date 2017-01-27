36a612cc9befced8b7a7a8881bb531fb0aae291e
Schoen Cho <fourthbus@gmail.com>
2017-01-27 00:17:29 +0000

    progressing 1C

--- Cam Files/floodsystem/geo2.py
+++ Cam Files/floodsystem/geo2.py
@@ -9,11 +9,18 @@ from haversine import haversine
 
 def stations_by_distance(stations, p):
 	#compte distance
-	distancelist=[]
+	stations_by_distance=[]
 	for station in stations:
 		#use haversine function
 		distance=haversine(station.coord, p)
 		#create a list of tuples contain data of stations
-		distancelist.append ((station.name,station.town,distance))
+		stations_by_distance.append ((station.name,station.town,distance))
 		#output the sorted
-	return sorted_by_key (distancelist,2,0)
+	return sorted_by_key (stations_by_distance,2,0)
+
+def stations_within_radius(stations, centre, r):
+	stations_within_radius=[]
+	distancelist=stations_by_distance(stations, centre)
+	for station in stations:
+		if station[2] < r
+		stations_within_radius.append ()
