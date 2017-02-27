c4be232b6ecfce6df4c9960ee2040d22165ca216
Schoen Cho <fourthbus@gmail.com>
2017-02-27 14:32:15 +0000

    move update levels to flood

--- Cam Files/Task2C.py
+++ Cam Files/Task2C.py
@@ -3,7 +3,6 @@ from floodsystem.flood import stations_highest_rel_level
 
 def run():
     stations = build_station_list()
-    update_water_levels(stations)
     list_of_tuples=stations_highest_rel_level(stations,10)
     for station in list_of_tuples:
     	print(station[0],station[1])
