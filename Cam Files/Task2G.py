43f1410b6a8ea3c3ccee63e7fee96c46a37b7f72
Schoen Cho <fourthbus@gmail.com>
2017-02-28 17:36:09 +0000

    2G

--- Cam Files/Task2G.py
+++ Cam Files/Task2G.py
@@ -1,5 +1,13 @@
 'This document is the warning system'
+from floodsystem.analysis import polyfit
+import numpy as np
 #part1
 #part2
+for station_low in stations_low:
+	dt = 1
+    dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+    poly, d0 = polyfit(dates, levels, p)
+    #make lists of date and lavel
+        plot_water_level_with_fit(station.name, dates, levels, 4)
 #part3
 #part4
