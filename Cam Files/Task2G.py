a86e438c4011466e61bafa5722f8fa71b51f68dd
Donald Ong <mho24@cam.ac.uk>
2017-02-28 18:14:52 +0000

    2G
    Separated stations into 3 groups

--- Cam Files/Task2G.py
+++ Cam Files/Task2G.py
@@ -1,37 +1,35 @@
 'This document is the warning system'
-from floodsystem.analysis import 
 import numpy as np
-from floodsystem.stationdata import build_station_list, update_water_levels, polyfit
+from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_highest_rel_level, stations_level_over_threshold
 #part1
-#return list of stations with relative water level above and below 0.8
+#return list of stations with different relative water levels
 stations= build_station_list()
 update_water_levels(stations)
-stations_name_high=[]
 stations_high=[]
+stations_mid=[]
 stations_low=[]
-test=[]
-list_of_tuples=stations_level_over_threshold(stations,0.8)
-for station in list_of_tuples:
-	stations_name_high.append(station[0])
+stations_none=[]
 for station in stations:
-	if (station.name in stations_name_high)==True:
-		stations_high.append(station)
+	if  station.relative_water_level()==None:
+			stations_none.append(station)
 	else:
-		pass
-
-for station in stations:
-	if (station.name in stations_name_high)==True:
-		pass
-	else:
-		stations_low.append(station)
+		if station.relative_water_level() < 0.8:
+			stations_low.append(station)
+		elif station. relative_water_level() >1.5:
+			stations_high.append(station)
+		else:
+			stations_mid.append(station)
+print(len(stations_low)+len(stations_mid)+len(stations_high)+len(stations_none))
+print(len(stations))
 
 #part2
-for station_low in stations_low:
-	dt = 1
-    dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
-    poly, d0 = polyfit(dates, levels, p)
-    #make lists of date and lavel
-        plot_water_level_with_fit(station.name, dates, levels, 4)
+#for station_low in stations_low:
+#	dt = 1
+#	dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+#	poly, d0 = polyfit(dates, levels, p)
+	#make lists of date and lavel
+#		plot_water_level_with_fit(station.name, dates, levels, 4)
 #part3
+
 #part4
