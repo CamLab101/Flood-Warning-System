4dee54ae8641c077989a87b89b85b00cf9b899e2
Schoen Cho <fourthbus@gmail.com>
2017-02-28 16:20:07 +0000

    2F

--- Cam Files/floodsystem/plot.py
+++ Cam Files/floodsystem/plot.py
@@ -5,6 +5,7 @@ from floodsystem.analysis import polyfit
 import matplotlib.dates
 import datetime
 import numpy as np
+from floodsystem.stationdata import build_station_list
 
 def plot_water_levels(station, dates, levels):
 	t = dates
@@ -24,6 +25,7 @@ def plot_water_levels(station, dates, levels):
 	plt.show()
 
 def plot_water_level_with_fit(station, dates, levels, p):
+
 	if dates == []:
 		print('{} station has no data to plot.'.format(station))
 	else:
@@ -31,9 +33,19 @@ def plot_water_level_with_fit(station, dates, levels, p):
 		level = levels
 		f, d0=polyfit(dates, levels, p)
 
+		#range line
+		stations=build_station_list()
+		for station_ind in stations:
+			if station_ind.name == station:
+				typical_low=np.linspace(station_ind.typical_range[0],station_ind.typical_range[0],len(t))
+				typical_high=np.linspace(station_ind.typical_range[1],station_ind.typical_range[1],len(t))
+
 		# Plots
 		plt.plot(t, level)
 		plt.plot(t, f(np.array(matplotlib.dates.date2num(t))-d0))
+		plt.plot(t, typical_low)
+		plt.plot(t, typical_high)
+
 		# Add axis labels, rotate date labels and add plot title
 		plt.xlabel('date')
 		plt.ylabel('water level (m)')
